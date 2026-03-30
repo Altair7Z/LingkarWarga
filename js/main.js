00bc77733a7f6c7abbc15c7c01fa64bf20dfc276
Altair7Z <faizg781hy@gmail.com>
2026-03-30 23:04:07 +0700

    update js dan css beserta html

---
+++ js/main.js
@@ -0,0 +1,124 @@
+let laporan = [];
+let pilihan = "Infrastruktur";
+let edit = false;
+
+let sedangEdit = null;
+
+function read() {
+  const listLaporan = document.getElementById("dataLaporan");
+  listLaporan.innerHTML = "";
+  const listData = document.getElementById("tabelAdmin");
+  listData.innerHTML = "";
+
+  laporan
+    .filter((i) => i.kategori === pilihan)
+    .forEach((tbl) => {
+      listLaporan.innerHTML += `
+        <div>
+            <strong>${tbl.judul}</strong><br>
+            <small>Pelapor: ${tbl.nama}</small><br>
+            <span>${tbl.status}</span>
+        </div>
+        <hr>    
+        `;
+    });
+
+  laporan.forEach((tbl) => {
+    listData.innerHTML += `
+        <tr>
+            <td>${tbl.id}</td>
+            <td>${tbl.nama}</td>
+            <td>${tbl.judul}</td>
+            <td>${tbl.status}</td>
+            <td>${tbl.waktu}</td>
+            <td>
+                <button type="button" onclick="update('${tbl.id}')">Edit</button>
+                <button onclick="hapusLaporan('${tbl.id}')">Hapus</button>
+            </td>
+        </tr>
+        `;
+  });
+}
+
+// create
+let form = document.getElementById("pengisian");
+form.addEventListener("submit", (a) => {
+  a.preventDefault();
+  const namaFix = document.getElementById("nama").value.toUpperCase();
+
+  if (edit && sedangEdit !== null) {
+    sedangEdit.nama = namaFix;
+    sedangEdit.judul = document.getElementById("judul-lap").value;
+    sedangEdit.kategori = document.getElementById("kategori").value;
+    sedangEdit.status = document.getElementById("statusInput").value;
+
+    alert("Perubahan di simpan");
+  } else {
+    const tgl = new Date();
+    const waktuString =
+      tgl.getDate() + "-" + (tgl.getMonth() + 1) + "-" + tgl.getFullYear();
+    const idBaru = "ID- " + Math.floor(Math.random() * 100);
+
+    laporan.push({
+      id: idBaru,
+      nama: namaFix,
+      judul: document.getElementById("judul-lap").value,
+      kategori: document.getElementById("kategori").value,
+      status: document.getElementById("statusInput").value,
+      waktu: waktuString,
+    });
+  }
+  resetForm();
+  read();
+});
+
+// update data
+function update(id) {
+  for (let i = 0; i < laporan.length; i++) {
+    if (laporan[i].id === id) {
+      sedangEdit = laporan[i];
+      edit = true;
+      break;
+    }
+  }
+
+  document.getElementById("nama").value = sedangEdit.nama;
+  document.getElementById("judul-lap").value = sedangEdit.judul;
+  document.getElementById("kategori").value = sedangEdit.kategori;
+  document.getElementById("statusInput").value = sedangEdit.status;
+
+  sedangEdit = true;
+  document.getElementById("btn-submit").innerText = "Simpan Perubahan";
+  document.getElementById("btn-batal").style.display = "inline-block";
+
+  document.getElementById("pengisian").scrollIntoView();
+}
+
+// hapusdata
+function hapusLaporan(id) {
+  if (confirm("Hapus laporan ini?")) {
+    laporan = laporan.filter((i) => i.id !== id);
+    read();
+  }
+}
+
+function resetForm() {
+  edit = false;
+  sedangEdit = null;
+
+  document.getElementById("pengisian").reset();
+  document.getElementById("btn-submit").innerText = "Kirim Laporan";
+  document.getElementById("btn-batal").style.display = "none";
+}
+
+function filter(opsi, el) {
+  pilihan = opsi;
+  document
+    .querySelectorAll(".btn-filter")
+    .forEach((b) => b.classList.remove("active"));
+  el.classList.add("active");
+  read();
+}
+
+document.getElementById("btn-batal").onclick = resetForm;
+read();
